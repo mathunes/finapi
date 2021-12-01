e918214c7ccae8d41efbd210596d953af84efd64
mathunes <matheusantunes720@gmail.com>
2021-12-01 10:42:30 -0300

    Add new routes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const express = require('express');
 const { v4:uuidv4 } = require('uuid');
 
@@ -22,6 +22,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
     request.customer = customer;
 
     return next();
+}
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === "credit") {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -61,14 +73,84 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     const statementOperation = {
         description,
         amount,
-        created_ad: new Date(),
+        created_at: new Date(),
         type: "credit"
     };
 
     customer.statement.push(statementOperation);
 
     return response.status(201).send(customer.statement);
-    
+
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if (parseInt(balance) < parseInt(amount)) {
+
+        return response.status(400).json({error: "Insufficient funds!"});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    console.log(dateFormat);
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date (dateFormat).toDateString());
+
+    return response.json(statement);
+
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.status(200).json(balance);
 });
 
 app.listen(3333);
